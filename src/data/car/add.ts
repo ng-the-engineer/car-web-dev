d91bc7a93fa1010200adaacadcfeacc99b627bfb
Anthony Ng <engineer.anthony.ng@gmail.com>
2021-10-17 09:20:54 +0100

    Removed unused files

--- src/data/car/add.ts
+++
@@ -1,9 +0,0 @@
-import { Car } from "./model"
-
-const add = (car: Car) : Car => {
-    // save car
-    const savedCar : Car = car  // id will be created on car
-    return savedCar
-}
-
-export default add
